b47c1e74bfc5fc849765281512202cb0c14f030d
Vikash Anand <anandin02@gmail.com>
2021-01-31 23:34:00 +0530

    Initial Commit

---
+++ MediumFollowers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MediumFollowers
+//
+//  Created by Vikash Anand on 31/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MediumFollowers/MediumFollowersApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediumFollowersApp.swift
+//  MediumFollowers
+//
+//  Created by Vikash Anand on 31/01/21.
+//
+
+import SwiftUI
+
+@main
+struct MediumFollowersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
